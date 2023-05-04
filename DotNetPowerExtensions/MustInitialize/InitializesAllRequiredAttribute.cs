d86a0f76acbb1e789e791af15f5e367a52541d1a
yoelhalb-hitech <yoelhalb@hitechmerchants.com>
2023-05-03 22:08:14 -0400

    Update shipped rules

--- DotNetPowerExtensions/MustInitialize/InitializesAllRequiredAttribute.cs
+++ DotNetPowerExtensions/MustInitialize/InitializesAllRequiredAttribute.cs
@@ -2,7 +2,7 @@
 namespace SequelPay.DotNetPowerExtensions;
 
 /// <summary>
-/// Specifies that the given Constructor is initializing all properties and fields marked with <see cref="MustInitializeAttribute"/> and does not caller initialization
+/// Specifies that the given Constructor is initializing all members marked with <see cref="MustInitializeAttribute"/> and does not caller initialization
 /// </summary>
 [AttributeUsage(AttributeTargets.Constructor)]
 public class InitializesAllRequiredAttribute : Attribute
--- DotNetPowerExtensions/MustInitialize/InitializesAttribute.cs
+++ DotNetPowerExtensions/MustInitialize/InitializesAttribute.cs
@@ -2,12 +2,15 @@
 namespace SequelPay.DotNetPowerExtensions;
 
 /// <summary>
-/// Specifies that the given Constructor is initializing all properties and fields marked with <see cref="MustInitializeAttribute"/> and does not caller initialization
+/// Specifies that the given Constructor is initializing the given members marked with <see cref="MustInitializeAttribute"/> and does not caller initialization
 /// </summary>
 [AttributeUsage(AttributeTargets.Constructor)]
 public class InitializesAttribute : Attribute
 {
     public InitializesAttribute(string member, params string[] members)
     {
+        Members = new[] {member}.Concat(members).ToArray();
     }
+
+    internal virtual string[] Members { get; set; }
 }
