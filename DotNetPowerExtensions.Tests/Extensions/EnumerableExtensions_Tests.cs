eded46a24f08622490aa4ea07e1be21cda363e8a
yoelhalb-hitech <yoelhalb@hitechmerchants.com>
2024-02-09 14:22:57 -0500

    Add EnumerableExtensions

---
+++ DotNetPowerExtensions.Tests/Extensions/EnumerableExtensions_Tests.cs
@@ -0,0 +1,34 @@
+﻿
+using SequelPay.DotNetPowerExtensions;
+
+namespace DotNetPowerExtensions.Tests.Extensions;
+
+internal class EnumerableExtensions_Tests
+{
+    [Test]
+    public void Test_Empty()
+    {
+        Assert.Throws<ArgumentNullException>(() => (null as int[])!.Empty());
+        Array.Empty<int>().Empty().Should().BeTrue();
+        new int[] {123}.Empty().Should().BeFalse();
+        new int[] { 123, 456 }.Empty().Should().BeFalse();
+    }
+
+    [Test]
+    public void Test_NullOrEmpty()
+    {
+        (null as int[]).NullOrEmpty().Should().BeTrue();
+        Array.Empty<int>().NullOrEmpty().Should().BeTrue();
+        new int[] { 123 }.NullOrEmpty().Should().BeFalse();
+        new int[] { 123, 456 }.NullOrEmpty().Should().BeFalse();
+    }
+
+    [Test]
+    public void Test_HasOnlyOne()
+    {
+        (null as int[]).HasOnlyOne().Should().BeFalse();
+        Array.Empty<int>().HasOnlyOne().Should().BeFalse();
+        new int[] { 123 }.HasOnlyOne().Should().BeTrue();
+        new int[] { 123, 456 }.HasOnlyOne().Should().BeFalse();
+    }
+}
---
+++ DotNetPowerExtensions/Extensions/EnumerableExtensions.cs
@@ -0,0 +1,31 @@
+﻿
+namespace SequelPay.DotNetPowerExtensions;
+
+public static class EnumerableExtensions
+{
+    /// <summary>
+    /// Determines whether a sequence <see cref="IEnumerable"/> is empty
+    /// </summary>
+    /// <typeparam name="TSource">The type of the elements of source.</typeparam>
+    /// <param name="source">The System.Collections.Generic.IEnumerable`1 to check for emptiness.</param>
+    /// <returns>true if the source sequence does not contain any elements; otherwise, false.</returns>
+    /// <exception cref="ArgumentNullException">source is null.</exception>
+    public static bool Empty<TSource>(this IEnumerable<TSource> source) => !source.Any();
+
+    /// <summary>
+    /// Determines whether a sequence <see cref="IEnumerable"/> is either null or empty
+    /// </summary>
+    /// <typeparam name="TSource">The type of the elements of source.</typeparam>
+    /// <param name="source">The System.Collections.Generic.IEnumerable`1 to check for null or emptiness.</param>
+    /// <returns>true if the source sequence is null or does not contain any elements; otherwise, false.</returns>
+    public static bool NullOrEmpty<TSource>(this IEnumerable<TSource>? source) => source?.Any() != true;
+
+    /// <summary>
+    /// Determines whether a sequence <see cref="IEnumerable"/> has exactly one element
+    /// </summary>
+    /// <typeparam name="TSource">The type of the elements of source.</typeparam>
+    /// <param name="source">The System.Collections.Generic.IEnumerable`1 to check for elements.</param>
+    /// <returns>true if the source sequence is not null and contains exactly one element; otherwise, false.</returns>
+    public static bool HasOnlyOne<TSource>(this IEnumerable<TSource>? source) => source?.NullOrEmpty() != true && source?.Skip(1).Empty() == true;
+
+}
